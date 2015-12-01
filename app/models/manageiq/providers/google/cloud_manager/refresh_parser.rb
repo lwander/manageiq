3dc3886d91f535bc8b0e498634b793531e42e2b2
Lars Wander <lars.wander@gmail.com>
2015-12-01 17:10:26 -0500

    Moved zone_uid tag for layout consistency

--- app/models/manageiq/providers/google/cloud_manager/refresh_parser.rb
+++ app/models/manageiq/providers/google/cloud_manager/refresh_parser.rb
@@ -164,6 +164,7 @@ module ManageIQ::Providers
           :raw_power_state   => instance.state,
           :flavor            => flavor,
           :availability_zone => zone,
+          :zone_uid          => zone_uid,
           :hardware          => {
             :cpu_sockets          => flavor[:cpus],
             :cpu_total_cores      => flavor[:cpu_cores],
@@ -172,7 +173,6 @@ module ManageIQ::Providers
             :disks                => [],
             :networks             => [],
           }
-          :zone_uid          => zone_uid,
         }
 
         return uid, new_result
